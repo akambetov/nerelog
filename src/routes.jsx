c60119a2f51fa0da4c7930956689bb743f6f5103
akambetov <almat_kambetov@mail.ru>
2022-05-14 19:34:22 +0600

    fix: nename routes.JS to routes.JSX

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,10 @@
+import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
 import MainPage from './features/main-page'
 
 export default () => (
     <Routes>
-        {' '}
         <Route path="/" element={<MainPage />} />
     </Routes>
 )
