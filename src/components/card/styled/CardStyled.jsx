e14ed91a968a84eb667bcd26b4bae0f2d4eac442
akambetov <almat_kambetov@mail.ru>
2022-05-15 01:41:06 +0600

    feat: card-container media

--- src/components/card/styled/CardStyled.jsx
+++ src/components/card/styled/CardStyled.jsx
@@ -3,7 +3,9 @@ import propTypes from 'prop-types'
 
 import style from './style.scss'
 
-export const CardStyled = ({ children }) => <div className={style.container}>{children}</div>
+export const CardStyled = ({ children }) => (
+    <div className={style['card-container']}>{children}</div>
+)
 
 CardStyled.propTypes = {
     children: propTypes.node
