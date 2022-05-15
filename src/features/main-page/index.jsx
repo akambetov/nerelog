7bd3f61d66ce0e9e1ee51f725382c38782cec248
akambetov <almat_kambetov@mail.ru>
2022-05-15 13:34:41 +0600

    feat: render mock data

--- src/features/main-page/index.jsx
+++ src/features/main-page/index.jsx
@@ -1,12 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState, useMemo } from 'react'
 
 import ScreenLayout from '../../components/screen-layout'
 import Card from '../../components/card'
 import { Flex } from '../../components/flex'
 import { List } from '../../components/list'
 
+import appsData from '../../mock-data/NeRelog_apps.json'
+import clientsData from '../../mock-data/NeRelog_clients.json'
+
 export default () => {
+    const [apps] = useState(appsData)
+    const [clients] = useState(clientsData)
     const [flexDirection, setFlexDirection] = useState(window.innerWidth < 720 ? 'column' : 'row')
+
+    const mappedData = useMemo(
+        () =>
+            apps.map((app) => {
+                const clientItem = clients.find((client) => client.id === app.client_id)
+
+                return { appData: app, clientData: clientItem }
+            }),
+        [apps, clients]
+    )
+
+    const formatter = new Intl.NumberFormat(['kk', 'ru-RU'], {
+        style: 'currency',
+        currency: 'KZT',
+        minimumFractionDigits: 0,
+        currencyDisplay: 'narrowSymbol'
+    })
 
     window.addEventListener('resize', () => {
         setFlexDirection(window.innerWidth < 720 ? 'column' : 'row')
@@ -19,90 +41,15 @@ export default () => {
                 <h1>Content</h1>
                 <Flex direction={flexDirection}>
                     <List>
-                        <Card>
-                            <Card.Title title="card title" />
-                            <Card.Content content="card conasssss ssssssssstent ssssssssstent ssssssssstent ssssssssstent ssssssssstent ssssssssstent ssssssssstent ssssssssstent " />
-                        </Card>
-                        <Card>
-                            <Card.Title title="card title" />
-                            <Card.Content content="card conasssss ssssssssstent ssssssssstent ssssssssstent ssssssssstent ssssssssstent ssssssssstent ssssssssstent ssssssssstent " />
-                        </Card>
-                        <Card>
-                            <Card.Title title="card title" />
-                            <Card.Content content="card conasssss ssssssssstent ssssssssstent ssssssssstent ssssssssstent ssssssssstent ssssssssstent ssssssssstent ssssssssstent " />
-                        </Card>
-                        <Card>
-                            <Card.Title title="card title" />
-                            <Card.Content content="card conasssss ssssssssstent ssssssssstent ssssssssstent ssssssssstent ssssssssstent ssssssssstent ssssssssstent ssssssssstent " />
-                        </Card>
-                        <Card>
-                            <Card.Title title="card title" />
-                            <Card.Content content="card conasssss ssssssssstent ssssssssstent ssssssssstent ssssssssstent ssssssssstent ssssssssstent ssssssssstent ssssssssstent " />
-                        </Card>
-                        <Card>
-                            <Card.Title title="card title" />
-                            <Card.Content content="card conasssss ssssssssstent ssssssssstent ssssssssstent ssssssssstent ssssssssstent ssssssssstent ssssssssstent ssssssssstent " />
-                        </Card>
-                        <Card>
-                            <Card.Title title="card title" />
-                            <Card.Content content="card conasssss ssssssssstent ssssssssstent ssssssssstent ssssssssstent ssssssssstent ssssssssstent ssssssssstent ssssssssstent " />
-                        </Card>
-                        <Card>
-                            <Card.Title title="card title" />
-                            <Card.Content content="card conasssss ssssssssstent ssssssssstent ssssssssstent ssssssssstent ssssssssstent ssssssssstent ssssssssstent ssssssssstent " />
-                        </Card>
+                        {mappedData.map((data) => (
+                            <Card key={data.appData.id}>
+                                <Card.Title title={data.clientData.name} />
+                                <Card.Content content={data.appData.type} />
+                                <Card.Content content={formatter.format(data.appData.price)} />
+                            </Card>
+                        ))}
                     </List>
                     <div>
-                        <p>
-                            Cupidatat aute aliqua velit commodo est deserunt quis minim in aute.
-                            Veniam occaecat amet ullamco laboris in incididunt reprehenderit id ut.
-                            Commodo aliqua ullamco ex ea velit qui aute tempor fugiat aliquip velit.
-                            Sint culpa et enim nulla proident ipsum quis aute nostrud dolore. Labore
-                            magna eiusmod officia nisi labore non aute commodo excepteur et laboris
-                            laborum ullamco laborum. Adipisicing deserunt minim esse consectetur
-                            incididunt amet exercitation anim ea. Reprehenderit aliqua qui sint
-                            fugiat Lorem non cupidatat sit eiusmod ullamco in.
-                        </p>
-                        <p>
-                            Cupidatat aute aliqua velit commodo est deserunt quis minim in aute.
-                            Veniam occaecat amet ullamco laboris in incididunt reprehenderit id ut.
-                            Commodo aliqua ullamco ex ea velit qui aute tempor fugiat aliquip velit.
-                            Sint culpa et enim nulla proident ipsum quis aute nostrud dolore. Labore
-                            magna eiusmod officia nisi labore non aute commodo excepteur et laboris
-                            laborum ullamco laborum. Adipisicing deserunt minim esse consectetur
-                            incididunt amet exercitation anim ea. Reprehenderit aliqua qui sint
-                            fugiat Lorem non cupidatat sit eiusmod ullamco in.
-                        </p>
-                        <p>
-                            Cupidatat aute aliqua velit commodo est deserunt quis minim in aute.
-                            Veniam occaecat amet ullamco laboris in incididunt reprehenderit id ut.
-                            Commodo aliqua ullamco ex ea velit qui aute tempor fugiat aliquip velit.
-                            Sint culpa et enim nulla proident ipsum quis aute nostrud dolore. Labore
-                            magna eiusmod officia nisi labore non aute commodo excepteur et laboris
-                            laborum ullamco laborum. Adipisicing deserunt minim esse consectetur
-                            incididunt amet exercitation anim ea. Reprehenderit aliqua qui sint
-                            fugiat Lorem non cupidatat sit eiusmod ullamco in.
-                        </p>
-                        <p>
-                            Cupidatat aute aliqua velit commodo est deserunt quis minim in aute.
-                            Veniam occaecat amet ullamco laboris in incididunt reprehenderit id ut.
-                            Commodo aliqua ullamco ex ea velit qui aute tempor fugiat aliquip velit.
-                            Sint culpa et enim nulla proident ipsum quis aute nostrud dolore. Labore
-                            magna eiusmod officia nisi labore non aute commodo excepteur et laboris
-                            laborum ullamco laborum. Adipisicing deserunt minim esse consectetur
-                            incididunt amet exercitation anim ea. Reprehenderit aliqua qui sint
-                            fugiat Lorem non cupidatat sit eiusmod ullamco in.
-                        </p>
-                        <p>
-                            Cupidatat aute aliqua velit commodo est deserunt quis minim in aute.
-                            Veniam occaecat amet ullamco laboris in incididunt reprehenderit id ut.
-                            Commodo aliqua ullamco ex ea velit qui aute tempor fugiat aliquip velit.
-                            Sint culpa et enim nulla proident ipsum quis aute nostrud dolore. Labore
-                            magna eiusmod officia nisi labore non aute commodo excepteur et laboris
-                            laborum ullamco laborum. Adipisicing deserunt minim esse consectetur
-                            incididunt amet exercitation anim ea. Reprehenderit aliqua qui sint
-                            fugiat Lorem non cupidatat sit eiusmod ullamco in.
-                        </p>
                         <p>
                             Cupidatat aute aliqua velit commodo est deserunt quis minim in aute.
                             Veniam occaecat amet ullamco laboris in incididunt reprehenderit id ut.
