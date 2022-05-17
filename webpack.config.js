f9f3c6dc9a1524d13526880d3cc93ee2d5375a04
akambetov <almat_kambetov@mail.ru>
2022-05-17 11:11:26 +0600

    fix: improve webpack configs

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,12 @@ module.exports = {
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
-                use: ['babel-loader']
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
             },
             {
                 test: /\.s(a|c)ss$/,
@@ -51,6 +56,18 @@ module.exports = {
                     },
                     {
                         loader: 'sass-loader',
+                        options: {
+                            sourceMap: !isProdaction
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    isProdaction ? MiniCssExtractPlugin.loader : 'style-loader',
+                    {
+                        loader: 'css-loader',
                         options: {
                             sourceMap: !isProdaction
                         }
