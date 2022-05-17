d07d497a54e910ae71723374a8c2dfdd6009aeaa
akambetov <almat_kambetov@mail.ru>
2022-05-17 20:41:10 +0600

    feat: webpack file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -73,6 +73,14 @@ module.exports = {
                         }
                     }
                 ]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader'
+                    }
+                ]
             }
         ]
     }
