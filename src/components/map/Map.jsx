7813a5deddc7d535ba1a5f2984f6493e46833284
akambetov <almat_kambetov@mail.ru>
2022-05-17 11:12:39 +0600

    feat: <Map />

---
+++ src/components/map/Map.jsx
@@ -0,0 +1,32 @@
+import React, { useCallback } from 'react'
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+
+const style = {
+    width: '100%',
+    height: '300px'
+}
+
+export const Map = () => {
+    // create map
+    const createMap = useCallback(() => {
+        const mapNode = document.getElementById('map')
+        const map = L.map(mapNode).setView([51.505, -0.09], 13)
+
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution:
+                '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map)
+
+        L.marker([51.5, -0.09])
+            .addTo(map)
+            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+            .openPopup()
+    }, [])
+
+    window.addEventListener('load', createMap)
+
+    return <div id="map" style={style} />
+}
+
+export default Map
--- src/features/main-page/index.jsx
+++ src/features/main-page/index.jsx
@@ -3,6 +3,7 @@ import React, { useState, useMemo, useEffect } from 'react'
 import ScreenLayout from '../../components/screen-layout'
 import { Flex } from '../../components/flex'
 import { List } from '../../components/list'
+import { Map } from '../../components/map/Map'
 
 import appsData from '../../mock-data/NeRelog_apps.json'
 import clientsData from '../../mock-data/NeRelog_clients.json'
@@ -36,88 +37,7 @@ export default () => {
             <ScreenLayout.Content>
                 <Flex direction={flexDirection}>
                     <List list={mappedData} />
-                    <div>
-                        <p>
-                            Cupidatat aute aliqua velit commodo est deserunt quis minim in aute.
-                            Veniam occaecat amet ullamco laboris in incididunt reprehenderit id ut.
-                            Commodo aliqua ullamco ex ea velit qui aute tempor fugiat aliquip velit.
-                            Sint culpa et enim nulla proident ipsum quis aute nostrud dolore. Labore
-                            magna eiusmod officia nisi labore non aute commodo excepteur et laboris
-                            laborum ullamco laborum. Adipisicing deserunt minim esse consectetur
-                            incididunt amet exercitation anim ea. Reprehenderit aliqua qui sint
-                            fugiat Lorem non cupidatat sit eiusmod ullamco in.
-                        </p>
-                        <p>
-                            Cupidatat aute aliqua velit commodo est deserunt quis minim in aute.
-                            Veniam occaecat amet ullamco laboris in incididunt reprehenderit id ut.
-                            Commodo aliqua ullamco ex ea velit qui aute tempor fugiat aliquip velit.
-                            Sint culpa et enim nulla proident ipsum quis aute nostrud dolore. Labore
-                            magna eiusmod officia nisi labore non aute commodo excepteur et laboris
-                            laborum ullamco laborum. Adipisicing deserunt minim esse consectetur
-                            incididunt amet exercitation anim ea. Reprehenderit aliqua qui sint
-                            fugiat Lorem non cupidatat sit eiusmod ullamco in.
-                        </p>
-                        <p>
-                            Cupidatat aute aliqua velit commodo est deserunt quis minim in aute.
-                            Veniam occaecat amet ullamco laboris in incididunt reprehenderit id ut.
-                            Commodo aliqua ullamco ex ea velit qui aute tempor fugiat aliquip velit.
-                            Sint culpa et enim nulla proident ipsum quis aute nostrud dolore. Labore
-                            magna eiusmod officia nisi labore non aute commodo excepteur et laboris
-                            laborum ullamco laborum. Adipisicing deserunt minim esse consectetur
-                            incididunt amet exercitation anim ea. Reprehenderit aliqua qui sint
-                            fugiat Lorem non cupidatat sit eiusmod ullamco in.
-                        </p>
-                        <p>
-                            Cupidatat aute aliqua velit commodo est deserunt quis minim in aute.
-                            Veniam occaecat amet ullamco laboris in incididunt reprehenderit id ut.
-                            Commodo aliqua ullamco ex ea velit qui aute tempor fugiat aliquip velit.
-                            Sint culpa et enim nulla proident ipsum quis aute nostrud dolore. Labore
-                            magna eiusmod officia nisi labore non aute commodo excepteur et laboris
-                            laborum ullamco laborum. Adipisicing deserunt minim esse consectetur
-                            incididunt amet exercitation anim ea. Reprehenderit aliqua qui sint
-                            fugiat Lorem non cupidatat sit eiusmod ullamco in.
-                        </p>
-                        <p>
-                            Cupidatat aute aliqua velit commodo est deserunt quis minim in aute.
-                            Veniam occaecat amet ullamco laboris in incididunt reprehenderit id ut.
-                            Commodo aliqua ullamco ex ea velit qui aute tempor fugiat aliquip velit.
-                            Sint culpa et enim nulla proident ipsum quis aute nostrud dolore. Labore
-                            magna eiusmod officia nisi labore non aute commodo excepteur et laboris
-                            laborum ullamco laborum. Adipisicing deserunt minim esse consectetur
-                            incididunt amet exercitation anim ea. Reprehenderit aliqua qui sint
-                            fugiat Lorem non cupidatat sit eiusmod ullamco in.
-                        </p>
-                        <p>
-                            Cupidatat aute aliqua velit commodo est deserunt quis minim in aute.
-                            Veniam occaecat amet ullamco laboris in incididunt reprehenderit id ut.
-                            Commodo aliqua ullamco ex ea velit qui aute tempor fugiat aliquip velit.
-                            Sint culpa et enim nulla proident ipsum quis aute nostrud dolore. Labore
-                            magna eiusmod officia nisi labore non aute commodo excepteur et laboris
-                            laborum ullamco laborum. Adipisicing deserunt minim esse consectetur
-                            incididunt amet exercitation anim ea. Reprehenderit aliqua qui sint
-                            fugiat Lorem non cupidatat sit eiusmod ullamco in.
-                        </p>
-                        <p>
-                            Cupidatat aute aliqua velit commodo est deserunt quis minim in aute.
-                            Veniam occaecat amet ullamco laboris in incididunt reprehenderit id ut.
-                            Commodo aliqua ullamco ex ea velit qui aute tempor fugiat aliquip velit.
-                            Sint culpa et enim nulla proident ipsum quis aute nostrud dolore. Labore
-                            magna eiusmod officia nisi labore non aute commodo excepteur et laboris
-                            laborum ullamco laborum. Adipisicing deserunt minim esse consectetur
-                            incididunt amet exercitation anim ea. Reprehenderit aliqua qui sint
-                            fugiat Lorem non cupidatat sit eiusmod ullamco in.
-                        </p>
-                        <p>
-                            Cupidatat aute aliqua velit commodo est deserunt quis minim in aute.
-                            Veniam occaecat amet ullamco laboris in incididunt reprehenderit id ut.
-                            Commodo aliqua ullamco ex ea velit qui aute tempor fugiat aliquip velit.
-                            Sint culpa et enim nulla proident ipsum quis aute nostrud dolore. Labore
-                            magna eiusmod officia nisi labore non aute commodo excepteur et laboris
-                            laborum ullamco laborum. Adipisicing deserunt minim esse consectetur
-                            incididunt amet exercitation anim ea. Reprehenderit aliqua qui sint
-                            fugiat Lorem non cupidatat sit eiusmod ullamco in.
-                        </p>
-                    </div>
+                    <Map />
                 </Flex>
             </ScreenLayout.Content>
             <ScreenLayout.Footer>
