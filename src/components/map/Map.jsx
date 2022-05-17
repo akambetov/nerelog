638730ac12cf753617bf40f0ae6d0cf476232223
akambetov <almat_kambetov@mail.ru>
2022-05-17 21:24:28 +0600

    feat: markers cluster

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -4,6 +4,9 @@ import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 import icon from 'leaflet/dist/images/marker-icon.png'
 import iconShadow from 'leaflet/dist/images/marker-shadow.png'
+import 'leaflet.markercluster/dist/leaflet.markercluster'
+import 'leaflet.markercluster/dist/MarkerCluster.css'
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css'
 
 const style = {
     width: '100%',
@@ -12,28 +15,27 @@ const style = {
 
 export const Map = ({ coordsData }) => {
     const mapRef = useRef(null)
-    coordsData.length = 10
 
     // create map
     const createMap = useCallback(() => {
-        const map = L.map(mapRef.current).setView([43.238949, 76.889709], 13)
+        const map = L.map(mapRef.current).setView([43.238949, 76.889709], 12)
         const defaultIcon = L.icon({
             iconUrl: icon,
             shadowUrl: iconShadow
         })
+        const markersGroup = L.markerClusterGroup({ chunkedLoading: true })
+        const markers = coordsData.map(({ coords, oredrInfo }) =>
+            L.marker([coords.lat, coords.long], { icon: defaultIcon }).bindPopup(
+                `${oredrInfo.id}.<br>${oredrInfo.clinetName}.`
+            )
+        )
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution:
                 '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map)
-
-        const markers = coordsData.map(({ coords, oredrInfo }) =>
-            L.marker([coords.lat, coords.long], { icon: defaultIcon })
-                .addTo(map)
-                .bindPopup(`${oredrInfo.id}.<br>${oredrInfo.clinetName}.`)
-        )
-
-        L.layerGroup(markers).addTo(map)
+        markersGroup.addLayers(markers)
+        map.addLayer(markersGroup)
     }, [coordsData])
 
     window.addEventListener('load', createMap)
